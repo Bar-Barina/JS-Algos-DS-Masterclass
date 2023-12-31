f4c0985761ccf5d83bfd5814ad9e1274e48723ff
Bar Barina <itsbarbarina@gmail.com>
2023-12-31 14:02:51 +0200

    added get solution for DLL

---
+++ DS/DLL/get.js
@@ -0,0 +1,38 @@
+"use strict"
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+    this.prev = null
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null
+    let middle = this.length / 2
+    if (index <= middle) {
+      while (counter !== index) {
+        let counter = 0
+        let currNode = this.head
+        currNode = currNode.next
+        counter++
+      }
+    }
+    if (index >= middle) {
+      while (counter !== index) {
+        let counter = this.length - 1
+        let currNode = this.tail
+        currNode = currNode.prev
+        counter--
+      }
+    }
+    return currNode
+  }
+}
